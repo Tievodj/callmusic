60b46ac06fea6010b4c684b56b96830e7f45e42b
Tievo <hector.gonzalezg@cubava.cu>
2021-04-03 14:42:53 -0400

    primera prueba desde pv

--- handlers/private.py
+++ handlers/private.py
@@ -1,32 +1,54 @@
 from pyrogram import Client
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Voice
 
-from helpers.filters import other_filters2
+from helpers.filters import command, other_filters2
+from helpers.decorators import errors
+from helpers.errors import DurationLimitError
+from helpers.gets import get_url, get_file_name
+
+from os import path
 
 
-@Client.on_message(other_filters2)
-async def start(_, message: Message):
-    await message.reply_text(
-        f"""I am an open-source @CallsMusic bot, I let you play music in your group's voice chat.
+import callsmusic
 
-The commands I currently support are:
+import converter
+from downloaders import youtube
 
-/play - play the replied audio file or YouTube video
-/pause - pause the audio stream
-/resume - resume the audio stream
-/skip - skip the current audio stream
-/stop - clear the queue and remove the userbot from the call
-        """,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Group", url="https://t.me/callsmusicchat"
-                    ),
-                    InlineKeyboardButton(
-                        "Channel", url="https://t.me/callsmusic"
-                    )
-                ]
-            ]
+from config import DURATION_LIMIT, CHAT_ID
+
+
+
+
+
+
+
+
+
+
+@Client.on_message(command("play") & other_filters2)
+@errors
+async def play(_, message: Message):
+    audio = (message.reply_to_message.audio or message.reply_to_message.voice) if message.reply_to_message else None
+    url = get_url(message)
+
+    if audio:
+        if round(audio.duration / 60) > DURATION_LIMIT:
+            raise DurationLimitError(
+                f"Videos longer than {DURATION_LIMIT} minute(s) aren't allowed, the provided video is {audio.duration / 60} minute(s)"
+            )
+
+        file_name = get_file_name(audio)
+        file_path = await converter.convert(
+            (await message.reply_to_message.download(file_name))
+            if not path.isfile(path.join("downloads", file_name)) else file_name
         )
-    )
+    elif url:
+        file_path = await converter.convert(youtube.download(url))
+    else:
+        return await message.reply_text("You did not give me anything to play!")
+
+    if message.chat.id in callsmusic.pytgcalls.active_calls:
+        await message.reply_text(f"Queued at position {await callsmusic.queues.put(message.chat.id, file_path=file_path)}!")
+    else:
+        callsmusic.pytgcalls.join_group_call({CHAT_ID}, file_path)
+        await message.reply_text("Playing...")
